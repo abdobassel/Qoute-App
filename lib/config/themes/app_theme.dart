90934690f730673ce4103829d8ddd38d5f5a6d00
abdobassel <abdobassel@gmail.com>
2024-01-12 17:11:36 +0200

    font family

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:qoutes/core/utils/app_colors.dart';
+import 'package:qoutes/core/utils/app_strings.dart';
 
 ThemeData appTheme() {
   return ThemeData(
+      fontFamily: AppStrings.fontFamily,
       primaryColor: AppColors.primary,
       hintColor: AppColors.hint,
       brightness: Brightness.light,
       scaffoldBackgroundColor: Colors.white,
       textTheme: const TextTheme(
           labelLarge: TextStyle(
-              fontSize: 16, color: Colors.black, fontWeight: FontWeight.w500)));
+        fontSize: 16,
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+      )));
 }
--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,3 +1,4 @@
 class AppStrings {
   static const String appName = 'Qoutes';
+  static const String fontFamily = 'Tajawal';
 }
